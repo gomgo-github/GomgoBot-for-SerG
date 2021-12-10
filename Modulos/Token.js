6150c8deed3414ebe3241d94f2af046686b7a64a
BLUE UNITY <gomgounity@gmail.com>
2021-12-10 12:17:27 +0000

    Error arreglado, causa: Un cambio de repositorio proboco un error entre las versiones de los proyectos, por lo que faltaban referencias

--- Modulos/Token.js
+++ Modulos/Token.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-const Token = "OTExNzQ4MzQ0OTIwMDQzNTYw.YZl50A.XZ16Ehm0B6Hq0gTldtSTTVMedHM"
-=======
+const Token = "OTExNzQ4MzQ0OTIwMDQzNTYw.YZl50A.CgVM4p730YlAye-ZRH8L5TnUFos"
 const Token = "TOKEN"
->>>>>>> origin/Gaswarebot
 
 module.exports = {Token}
